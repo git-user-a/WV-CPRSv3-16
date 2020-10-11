3ce65d5b32a98d27d83ad471f1b74ff9cb64613c
AA <git.user.a@gmail.com>
2020-10-11 15:28:31 -0500

    ROR Post build updated to copy *.dcu in correct directory.

--- GMRV-5-0-38/CCR-COMPONENTS/PostBuild_D10_3_3.bat
+++ GMRV-5-0-38/CCR-COMPONENTS/PostBuild_D10_3_3.bat
@@ -2,9 +2,6 @@ if exist "%~1Resource" goto COPYFILES
 mkdir "%~1Resource"
 :COPYFILES
 copy dcu\*.dcu "%~1Resource"
-copy Source\OR2006Compatibility.dfm "%~1Resource"
-copy Source\ORDtTm.dfm "%~1Resource"
-copy Source\ORDtTmRng.dfm "%~1Resource"
-copy Source\ORCtrls.res "%~1Resource"
-copy Source\ORDtTm.res "%~1Resource"
+copy *.res "%~1Resource"
+copy fROR_PCALL.dfm "%~1Resource"
 rmdir dcu /s /q
